bfb96a2323131d4e587a88d046ee6996a2b521c7
MrValeVale <e01926@ncc.qld.edu.au>
2024-06-04 14:52:01 +1000

    Update login.php

--- login.php
+++ login.php
@@ -1,99 +1,48 @@
-<?php 
+<?php
+// Start the session
+session_start();
 
-//Connects to your Database 
-$conect = mysqli_connect("db location","username","password", "forks") or die(mysql_error()); 
+// Database connection settings
+$servername = "localhost";
+$username = "root"; // replace with your database username
+$password = ""; // replace with your database password
+$dbname = "my_database";
 
-//Checks if there is a login cookie
-if(isset($_COOKIE['ID_your_site'])){ //if there is, it logs you in and directes you to the members page
- 	$username = $_COOKIE['ID_your_site']; 
- 	$pass = $_COOKIE['Key_your_site'];
- 	$check = mysqli_query($conect, "SELECT * FROM users WHERE username = '$username'")or die(mysql_error());
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
 
- 	while($info = mysqli_fetch_array( $check )){
- 		if ($pass != $info['password']){}
- 		else{
- 			header("Location: login.php");
-		}
- 	}
- }
-
- //if the login form is submitted 
- if (isset($_POST['submit'])) {
-
-	// makes sure they filled it in
- 	if(!$_POST['username']){
- 		die('You did not fill in a username.');
- 	}
- 	if(!$_POST['pass']){
- 		die('You did not fill in a password.');
- 	}
-
- 	// checks it against the database
- 	if (!get_magic_quotes_gpc()){
- 		$_POST['email'] = addslashes($_POST['email']);
- 	}
-
- 	$check = mysqli_query($conect, "SELECT * FROM users WHERE username = '".$_POST['username']."'")or die(mysql_error());
-
- //Gives error if user dosen't exist
- $check2 = mysqli_num_rows($check);
- if ($check2 == 0){
-	die('That user does not exist in our database.<br /><br />If you think this is wrong <a href="login.php">try again</a>.');
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
 }
 
-while($info = mysqli_fetch_array( $check )){
-	$_POST['pass'] = stripslashes($_POST['pass']);
- 	$info['password'] = stripslashes($info['password']);
- 	$_POST['pass'] = md5($_POST['pass']);
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $user = $_POST['username'];
+    $pass = $_POST['password'];
 
-	//gives error if the password is wrong
- 	if ($_POST['pass'] != $info['password']){
- 		die('Incorrect password, please <a href="login.php">try again</a>.');
- 	}
-	
-	else{ // if login is ok then we add a cookie 
-		$_POST['username'] = stripslashes($_POST['username']); 
-		$hour = time() + 3600; 
-		setcookie(ID_your_site, $_POST['username'], $hour); 
-		setcookie(Key_your_site, $_POST['pass'], $hour);	 
- 
-		//then redirect them to the members area 
-		header("Location: members.php"); 
-	}
+    // Validate the inputs
+    if (!empty($user) && !empty($pass)) {
+        // Sanitize the inputs
+        $user = $conn->real_escape_string($user);
+        $pass = md5($conn->real_escape_string($pass)); // Password should be hashed in the same way as stored
+
+        // Query to check the credentials
+        $sql = "SELECT * FROM users WHERE username='$user' AND password='$pass'";
+        $result = $conn->query($sql);
+
+        if ($result->num_rows == 1) {
+            // Login successful
+            $_SESSION['username'] = $user;
+            header("Location: welcome.php"); // Redirect to a welcome page
+            exit();
+        } else {
+            // Login failed
+            echo "Invalid username or password.";
+        }
+    } else {
+        echo "Please enter both username and password.";
+    }
 }
-}
-else{
-// if they are not logged in 
+
+$conn->close();
 ?>
-
- <form action="<?php echo $_SERVER['PHP_SELF']?>" method="post"> 
-
- <table border="0"> 
-
- <tr><td colspan=2><h1>Login</h1></td></tr> 
-
- <tr><td>Username:</td><td> 
-
- <input type="text" name="username" maxlength="40"> 
-
- </td></tr> 
-
- <tr><td>Password:</td><td> 
-
- <input type="password" name="pass" maxlength="50"> 
-
- </td></tr> 
-
- <tr><td colspan="2" align="right"> 
-
- <input type="submit" name="submit" value="Login"> 
-
- </td></tr> 
-
- </table> 
-
- </form> 
-
- <?php 
- }
- ?> 
