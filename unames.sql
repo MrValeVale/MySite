92115853004cd5526e682fd39de15b53cbe1598c
MrValeVale <e01926@ncc.qld.edu.au>
2024-06-06 08:18:21 +1000

    Add files via upload

---
+++ unames.sql
@@ -0,0 +1,8 @@
+
+        create table calc(uname char, pass char);
+
+        insert into calc values('matt','pass1');
+        insert into calc values('John','pass1');
+        insert into calc values('Sam','pass1');
+
+        select * from calc;
